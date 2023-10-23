4b71b620809129bb9e283a1aa058203be558f87e
andreybel1989 <bav7269213@gmail.com>
2023-10-23 23:10:06 +0300

    second task

---
+++ Example002/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int InputNumber()
+{
+    Console.WriteLine("Input number");
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+
+
+int GetSumNumbers(int num)
+{
+    int result = 0;
+        
+    for (; num != 0;)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+
+    if (result < 0 ) result *= -1;
+    return result;
+}
+
+Console.WriteLine(GetSumNumbers(InputNumber()));
